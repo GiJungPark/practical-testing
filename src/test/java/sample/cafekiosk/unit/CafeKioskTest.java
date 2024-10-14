68f93981d18309f7ffdcc5801bd9278e75b2d782
GiJung <101462387+GiJungPark@users.noreply.github.com>
2024-10-15 00:40:51 +0900

    feat: 추가된 요구사항(한 음료 여러 잔 주문) 구현 및 테스트 코드 작성

--- src/test/java/sample/cafekiosk/unit/CafeKioskTest.java
+++ src/test/java/sample/cafekiosk/unit/CafeKioskTest.java
@@ -5,6 +5,7 @@ import sample.cafekiosk.unit.beverage.Americano;
 import sample.cafekiosk.unit.beverage.Latte;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
 class CafeKioskTest {
 
@@ -13,8 +14,28 @@ class CafeKioskTest {
         CafeKiosk cafeKiosk = new CafeKiosk();
         cafeKiosk.add(new Americano());
 
-        assertThat(cafeKiosk.getBeverages().size()).isEqualTo(1);
+        assertThat(cafeKiosk.getBeverages()).hasSize(1);
         assertThat(cafeKiosk.getBeverages().get(0).getName()).isEqualTo("아메리카노");
+    }
+
+    @Test
+    void addSeveralBeverages() {
+        CafeKiosk cafeKiosk = new CafeKiosk();
+        Americano americano = new Americano();
+        cafeKiosk.add(americano, 2);
+
+        assertThat(cafeKiosk.getBeverages().get(0)).isEqualTo(americano);
+        assertThat(cafeKiosk.getBeverages().get(1)).isEqualTo(americano);
+    }
+
+    @Test
+    void addZeroBeverages() {
+        CafeKiosk cafeKiosk = new CafeKiosk();
+        Americano americano = new Americano();
+
+        assertThatThrownBy(() -> cafeKiosk.add(americano, 0))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("음료는 1잔 이상 주문할 수 있습니다.");
     }
 
     @Test
@@ -23,7 +44,7 @@ class CafeKioskTest {
         Americano americano = new Americano();
 
         cafeKiosk.add(americano);
-        assertThat(cafeKiosk.getBeverages().size()).isEqualTo(1);
+        assertThat(cafeKiosk.getBeverages()).hasSize(1);
 
         cafeKiosk.remove(americano);
         assertThat(cafeKiosk.getBeverages()).isEmpty();
@@ -37,7 +58,7 @@ class CafeKioskTest {
 
         cafeKiosk.add(americano);
         cafeKiosk.add(latte);
-        assertThat(cafeKiosk.getBeverages().size()).isEqualTo(2);
+        assertThat(cafeKiosk.getBeverages()).hasSize(2);
 
         cafeKiosk.clear();
         assertThat(cafeKiosk.getBeverages()).isEmpty();
