af017d6afad95f745f411692a9b4fbd32563bb33
GiJung <101462387+GiJungPark@users.noreply.github.com>
2024-10-20 18:45:24 +0900

    test: 중복되는 상품번호 리스트로 주문을 생성하는 테스트 코드 작성

--- src/test/java/sample/cafekiosk/spring/api/service/order/OrderServiceTest.java
+++ src/test/java/sample/cafekiosk/spring/api/service/order/OrderServiceTest.java
@@ -62,6 +62,37 @@ class OrderServiceTest {
                         tuple("002", 3000)
                 );
 
+    }
+
+    @DisplayName("중복되는 상품번호 리스트로 주문을 생성할 수 있다.")
+    @Test
+    void createOrderWithDuplicateProductNumbers() {
+        // given
+        Product product1 = createProduct("001", HANDMADE, 1000);
+        Product product2 = createProduct("002", HANDMADE, 3000);
+        Product product3 = createProduct("003", HANDMADE, 5000);
+        productRepository.saveAll(List.of(product1, product2, product3));
+
+        OrderCreateRequest request = OrderCreateRequest.builder()
+                .productNumbers(List.of("001", "001"))
+                .build();
+
+        LocalDateTime registeredDateTime = LocalDateTime.now();
+
+        // when
+        OrderResponse orderResponse = orderService.createOrder(request, registeredDateTime);
+
+        // then
+        assertThat(orderResponse.getId()).isNotNull();
+        assertThat(orderResponse)
+                .extracting("registeredDateTime", "totalPrice")
+                .contains(registeredDateTime, 2000);
+        assertThat(orderResponse.getProducts()).hasSize(2)
+                .extracting("productNumber", "price")
+                .containsExactlyInAnyOrder(
+                        tuple("001", 1000),
+                        tuple("001", 1000)
+                );
 
     }
 
