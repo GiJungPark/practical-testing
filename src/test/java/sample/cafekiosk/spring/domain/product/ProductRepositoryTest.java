b300b3638380743f0fab87fc8fe132561f037828
GiJung <101462387+GiJungPark@users.noreply.github.com>
2024-10-20 18:02:05 +0900

    refactor: productNo 에서 prodcutNumber로 필드명 변경

--- src/test/java/sample/cafekiosk/spring/domain/product/ProductRepositoryTest.java
+++ src/test/java/sample/cafekiosk/spring/domain/product/ProductRepositoryTest.java
@@ -30,7 +30,7 @@ class ProductRepositoryTest {
     void findAllBySellingStatusIn() {
         // given
         Product americano = Product.builder()
-                .productNo("001")
+                .productNumber("001")
                 .type(HANDMADE)
                 .sellingType(SELLING)
                 .productName("아메리카노")
@@ -38,7 +38,7 @@ class ProductRepositoryTest {
                 .build();
 
         Product latte = Product.builder()
-                .productNo("002")
+                .productNumber("002")
                 .type(HANDMADE)
                 .sellingType(HOLD)
                 .productName("카페라떼")
@@ -46,7 +46,7 @@ class ProductRepositoryTest {
                 .build();
 
         Product shavedIce = Product.builder()
-                .productNo("003")
+                .productNumber("003")
                 .type(HANDMADE)
                 .sellingType(STOP_SELLING)
                 .productName("팥빙수")
@@ -60,7 +60,7 @@ class ProductRepositoryTest {
 
         // when
         assertThat(products).hasSize(2)
-                .extracting("productNo", "productName", "sellingType")
+                .extracting("productNumber", "productName", "sellingType")
                 .containsExactlyInAnyOrder(
                   tuple("001", "아메리카노", SELLING),
                   tuple("002", "카페라떼", HOLD)
