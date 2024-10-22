f878de1a5e2eb8ddc58f169987cbc46aa7a1f66f
GiJung <101462387+GiJungPark@users.noreply.github.com>
2024-10-23 02:42:41 +0900

    feat: Product Controller 테스트 작성

--- src/main/java/sample/cafekiosk/spring/api/controller/product/request/ProductCreateRequest.java
+++ src/main/java/sample/cafekiosk/spring/api/controller/product/request/ProductCreateRequest.java
@@ -1,18 +1,30 @@
 package sample.cafekiosk.spring.api.controller.product.request;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.Builder;
 import lombok.Getter;
-import sample.cafekiosk.spring.api.service.product.response.ProductResponse;
+import lombok.NoArgsConstructor;
 import sample.cafekiosk.spring.domain.product.Product;
 import sample.cafekiosk.spring.domain.product.ProductSellingType;
 import sample.cafekiosk.spring.domain.product.ProductType;
 
+@NoArgsConstructor
 @Getter
 public class ProductCreateRequest {
 
+    @NotNull(message = "상품 타입은 필수입니다.")
     private ProductType type;
+
+    @NotNull(message = "상품 판매 상태는 필수입니다.")
     private ProductSellingType sellingType;
+
+    @NotBlank(message = "상품 이름은 필수입니다.")
     private String productName;
+
+    @Positive(message = "상품 가격은 양수여야 합니다.")
     private int price;
 
     @Builder
@@ -25,11 +37,11 @@ public class ProductCreateRequest {
 
     public Product toEntity(String latestProductNumber) {
         return Product.builder()
-                .productNumber(latestProductNumber)
-                .type(type)
-                .sellingType(sellingType)
-                .productName(productName)
-                .price(price)
-                .build();
+            .productNumber(latestProductNumber)
+            .type(type)
+            .sellingType(sellingType)
+            .productName(productName)
+            .price(price)
+            .build();
     }
 }
